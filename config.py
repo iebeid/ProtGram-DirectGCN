2565611c6f3ca6505351a6f9b751530327829653
Islam Akef Ebeid <islam.akef@gmail.com>
2025-06-18 12:27:51 -0500

    Update config
    QUick

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ class Config:
         self.RUN_WORD2VEC_PIPELINE = False
         self.RUN_TRANSFORMER_PIPELINE = False
         self.RUN_BENCHMARKING_PIPELINE = False
-        self.RUN_MAIN_PPI_EVALUATION = True
+        self.RUN_MAIN_PPI_EVALUATION = False
         self.RUN_DUMMY_TEST = True
         self.CLEANUP_DUMMY_DATA = True
 
