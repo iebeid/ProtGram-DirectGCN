a30f263f957755e4541c75af17de91bfeb223e77
Islam Akef Ebeid <islam.akef@gmail.com>
2025-06-21 19:22:46 -0500

    Update config
    Config update

--- config.py
+++ config.py
@@ -100,9 +100,9 @@ class Config:
         self.GCN_CLUSTER_TRAINING_THRESHOLD_NODES = 10000  # Apply clustering for graphs with > 10k nodes
 
         # New parameters for automatic cluster count
-        self.GCN_TARGET_NODES_PER_CLUSTER = 500  # Aim for 500 nodes per cluster
+        self.GCN_TARGET_NODES_PER_CLUSTER = 50  # Aim for 500 nodes per cluster
         self.GCN_MIN_CLUSTERS = 2  # Ensure at least 2 clusters if clustering is enabled
-        self.GCN_MAX_CLUSTERS = 500  # Cap the number of clusters to avoid excessive fragmentation
+        self.GCN_MAX_CLUSTERS = 2000  # Cap the number of clusters to avoid excessive fragmentation
 
         self.POOLING_WORKERS: Optional[int] = max(1, os.cpu_count() - 4) if os.cpu_count() else 1
         self.APPLY_PCA_TO_GCN = True
