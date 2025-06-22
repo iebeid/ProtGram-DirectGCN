09e41849d3690700b70225ee928911510ab1be01
Islam Akef Ebeid <islam.akef@gmail.com>
2025-06-21 19:59:48 -0500

    Udpate config
    Update condig for run

--- config.py
+++ config.py
@@ -57,7 +57,7 @@ class Config:
         self.BENCHMARK_SPLIT_RATIOS: Dict[str, float] = {"train": 0.1, "val": 0.1, "test": 0.8}
 
         # --- 2. GCN PIPELINE PARAMETERS (Your custom GCN) ---
-        self.GCN_NGRAM_MAX_N = 1
+        self.GCN_NGRAM_MAX_N = 4
         self.GRAPH_BUILDER_WORKERS: Optional[int] = max(1, os.cpu_count() - 4) if os.cpu_count() else 1
 
         self.GCN_HIDDEN_LAYER_DIMS = [256, 128, 64]
@@ -88,15 +88,15 @@ class Config:
 
         self.GCN_TASK_TYPES_PER_LEVEL: Dict[int, str] = {
             1: "next_node",
-            # 2: "next_node",
-            # 3: "next_node",
-            # 4: "next_node"
+            2: "next_node",
+            3: "next_node",
+            4: "next_node"
         }
         self.GCN_DEFAULT_TASK_TYPE: str = "community"
         self.GCN_CLOSEST_AA_K_HOPS: int = 3
 
         # --- NEW: Cluster-GCN Training Strategy ---
-        self.GCN_USE_CLUSTER_TRAINING = False
+        self.GCN_USE_CLUSTER_TRAINING = True
         self.GCN_CLUSTER_TRAINING_THRESHOLD_NODES = 10000  # Apply clustering for graphs with > 10k nodes
 
         # New parameters for automatic cluster count
