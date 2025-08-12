5f683099c1932c36fdc9c7bb74cb908d18cc4b73
Islam Akef Ebeid <islam.akef@gmail.com>
2025-08-12 11:18:04 -0500

    Update 1F 8-12-25
    bug fix

--- source/trainers/singleton_xgcn.py
+++ source/trainers/singleton_xgcn.py
@@ -69,8 +69,9 @@ class SingletonXGCNTrainer:
         # This ensures that the model is always tested under similar input conditions,
         # even though the random features for n=1 are already somewhat normalized.
         bn = torch.nn.BatchNorm1d(initial_features.shape[1]).to(self.device)
-        # Move features to device for normalization, the rest of the function expects it there.
-        initial_features = bn(initial_features.to(self.device))
+        # Move features to device for normalization, then detach them from the computation graph.
+        # This prevents the "trying to backward through the graph a second time" error.
+        initial_features = bn(initial_features.to(self.device)).detach()
 
         node_indices = np.arange(self.graph.number_of_nodes)
         try:
