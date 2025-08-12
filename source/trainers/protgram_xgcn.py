c6b6d82373699f723a8eab8e1405bce250152784
Islam Akef Ebeid <islam.akef@gmail.com>
2025-08-12 17:57:48 -0500

    Update 3D 8-12-25
    bug fix

--- source/trainers/protgram_xgcn.py
+++ source/trainers/protgram_xgcn.py
@@ -33,7 +33,8 @@ from source.data_builders.xgcn import XGCNDataBuilder
 from source.experiments.ppi_1 import PPIPipeline
 from source.models.factory import ModelFactory
 from source.utils.data import DataUtils, IDMapGenerator, FastaUtils, ProtgramDaskHelpers
-from source.utils.post import EmbeddingProcessor, EarlyStopper
+from source.utils.post import EmbeddingProcessor
+from source.utils.models import EarlyStopper
 from source.utils.results import EvaluationReporter
 
 
