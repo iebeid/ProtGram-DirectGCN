3ac3e4daac18032ee900821851f2252d34325889
Islam Akef Ebeid <islam.akef@gmail.com>
2025-08-27 13:49:58 -0500

    Update 6 8-27-25
    bug fixes

--- source/utils/data/id_mapper.py
+++ source/utils/data/id_mapper.py
@@ -127,7 +127,7 @@ class IDMapper:
                     usecols=['uniprot_id', 'db_type', 'db_id'],
                     dtype={'uniprot_id': 'string', 'db_type': 'string', 'db_id': 'string'},
                     on_bad_lines='warn',
-                    blocksize='256MB'
+                    blocksize='64MB'
                 )
 
                 # --- DEFINITIVE FIX: Remove the incorrect filter ---
